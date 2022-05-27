ec4a8cd82e2b6ec64d3db718027e9c231a0b8301
Adam Ferla <aferla@gmail.com>
2022-05-27 20:02:53 +1000

    About to add webpack

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import * as p5 from "p5";
+
+function gameOfLife(p: p5){
+    // x = 100;
+    // y = 100;
+
+    // this.board = new GameBoard(640, 480);
+
+    p.setup = () => {
+        p.createCanvas(640, 480);
+    }
+
+    p.draw = () => {
+        p.background(p.color(200, 200, 200));
+        // this.board.draw(p);
+    }
+
+    // p.mouseClicked = () => {
+    //     this.board.handleClick({x: p.mouseX, y: p.mouseY});
+    // }
+}
+
+new p5(gameOfLife);
